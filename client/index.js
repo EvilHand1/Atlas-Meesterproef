381b12ad954b97f77d8a968ad2060f036683f9ca
decrek <declanrek@gmail.com>
2025-04-15 14:31:11 +0200

    add search on movies with client side enhancement

--- client/index.js
+++ client/index.js
@@ -1,3 +1,61 @@
 import './index.css';
 
 console.log('Hello, world!');
+
+const saveButtons = document.querySelectorAll('[data-save-button]')
+
+saveButtons.forEach(saveButton => {
+  saveButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const newFavouriteId = e.target.getAttribute('data-save-button');
+    const oldFavourites = JSON.parse(window.localStorage.getItem('favourites')) || [];
+    oldFavourites.push(newFavouriteId);
+    window.localStorage.setItem('favourites', JSON.stringify(oldFavourites))
+  })
+})
+
+
+const searchForm = document.querySelector('[data-search-form]');
+const searchresults = document.querySelector('[data-search-results]');
+const input = document.querySelector('[data-search-input]');
+
+let debounceTimeout;
+input.addEventListener('input', (e) => {
+  clearTimeout(debounceTimeout);
+  const value = e.target.value;
+  debounceTimeout = setTimeout(() => {
+    if (value.length > 3) {
+      fetchResults();
+    }
+  }, 300); // Adjust debounce delay (in ms) as needed
+});
+
+searchForm.addEventListener('change', (e) => {
+  e.preventDefault()
+  fetchResults()
+})
+
+function fetchResults() {
+  const formdata = new FormData(searchForm);
+
+  const queryString = new URLSearchParams(formdata).toString();
+  const url = `/search/?${queryString}`;
+
+  window.history.replaceState(null, null,url);
+
+  fetch(url, {
+    method: searchForm.method,
+    headers: {
+      'content-type': 'x-www-form-urlencoded',
+      'x-requested-with': 'XMLHttpRequest',
+    },
+  }).then(res => {
+    if (res.ok) {
+      return res.text();
+    }
+    throw new Error('Network response was not ok.');
+  })
+    .then(html => {
+      searchresults.innerHTML = html;
+    })
+}
--- server/server.js
+++ server/server.js
@@ -1,30 +1,10 @@
+import 'dotenv/config'
+
 import { App } from '@tinyhttp/app';
 import { logger } from '@tinyhttp/logger';
 import { Liquid } from 'liquidjs';
+import bodyParser from 'body-parser';
 import sirv from 'sirv';
-
-const data = {
-  'beemdkroon': {
-    id: 'beemdkroon',
-    name: 'Beemdkroon',
-    image: {
-      src: 'https://i.pinimg.com/736x/09/0a/9c/090a9c238e1c290bb580a4ebe265134d.jpg',
-      alt: 'Beemdkroon',
-      width: 695,
-      height: 1080,
-    }
-  },
-  'wilde-peen': {
-    id: 'wilde-peen',
-    name: 'Wilde Peen',
-    image: {
-      src: 'https://mens-en-gezondheid.infonu.nl/artikel-fotos/tom008/4251914036.jpg',
-      alt: 'Wilde Peen',
-      width: 418,
-      height: 600,
-    }
-  }
-}
 
 const engine = new Liquid({
   extname: '.liquid',
@@ -32,14 +12,47 @@ const engine = new Liquid({
 
 const app = new App();
 
+const favourites = []
+
 app
   .use(logger())
+  .use(bodyParser.urlencoded())
   .use('/', sirv('dist'))
   .listen(3000, () => console.log('Server available on http://localhost:3000'));
 
 app.get('/', async (req, res) => {
-  return res.send(renderTemplate('server/views/index.liquid', { title: 'Home', items: Object.values(data) }));
+  const movies = await fetchData('https://api.themoviedb.org/3/discover/movie')
+
+  const success = req.query.success;
+
+  return res.send(renderTemplate('server/views/index.liquid', { title: 'Home', movies: movies.results, success: success }));
 });
+
+app.get('/search/', async (req, res) => {
+  const query = req.query.q;
+  const movies = await fetchData('https://api.themoviedb.org/3/search/movie?query=' + query)
+
+  const requestedWithJs = req.headers['x-requested-with'] === 'XMLHttpRequest';
+
+  if (requestedWithJs) {
+    return res.send(renderTemplate('server/components/search-results/search-results.liquid', {
+      results: movies.results,
+    }));
+  } else {
+    return res.send(renderTemplate('server/views/search.liquid', {
+      title: 'Search',
+      query: query,
+      results: movies.results,
+      totalResults: movies.total_results
+    }));
+  }
+})
+
+app.post('/favourite/', async (req, res) => {
+  const body = req.body;
+  favourites.push(body.id)
+  return res.redirect(req.headers.referer + '?success=true');
+})
 
 app.get('/plant/:id/', async (req, res) => {
   const id = req.params.id;
@@ -56,3 +69,15 @@ app.get('/plant/:id/', async (req, res) => {
 const renderTemplate = (template, data) => {
   return engine.renderFileSync(template, data);
 };
+
+
+
+function fetchData(url) {
+  return fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${process.env.API_TOKEN}`
+    }
+  })
+    .then(response => response.json())
+}
